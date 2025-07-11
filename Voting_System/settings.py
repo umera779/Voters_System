d1bff4f1ae4e9a66a0fcf745185529f7d41accf7
umera779 <109315806+umera779@users.noreply.github.com>
2025-07-11 20:54:46 +0100

    Update settings.py

--- Voting_System/settings.py
+++ Voting_System/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-)z9(uif!qi!29^y8wz(jm5=_8=i7uc-ii1m3rv!v9eih*u0yxd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost','voters-system.onrender.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
