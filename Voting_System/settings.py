1dfa79017e30df5fb2f13ed38b6ffd6264a84a82
emmanuelumera <emmanuelumera107@gmail.com>
2025-08-05 07:15:26 -0400

    yes

--- Voting_System/settings.py
+++ Voting_System/settings.py
@@ -117,4 +117,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 LOGIN_REDIRECT_URL = '/'
 LOGIN_URL = 'login'
 LOGOUT_REDIRECT_URL = 'login'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
